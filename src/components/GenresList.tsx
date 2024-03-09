a12f5d3915ba4679f785626d95a39c6662616494
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-03-09 23:09:57 +0100

    Filter games by clicking on the genre links in the side menu.

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,8 +1,20 @@
-import useGenres from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import getCroppedImage from "../services/imag-url";
 
-const GenresList = () => {
+interface Props {
+  onSelectGenres: (genres: Genre) => void;
+}
+
+const GenresList = ({ onSelectGenres }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -19,7 +31,13 @@ const GenresList = () => {
               borderRadius={8}
               src={getCroppedImage(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenres(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
