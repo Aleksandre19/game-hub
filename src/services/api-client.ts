4dd3455aa7aa36f76002977446032fc29b2e0859
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-03-20 03:45:52 +0100

    Reverse a API Key set up.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-// import apiKey from "./api-key";
+import apiKey from "./api-key";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: process.env.REACT_APP_API_KEY,
+    key: apiKey,
   },
 });
