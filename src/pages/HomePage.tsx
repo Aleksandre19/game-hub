c60c4f1a68a8fadc79b996764d0e3d7513d86e92
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-03-25 23:36:54 +0100

    Set up Routing.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,44 @@
+import { Box, Grid, Show, GridItem, Flex } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenresList from "../components/GenresList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Grid
+        templateAreas={{
+          base: `"main"`,
+          lg: `"aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <Show above="lg">
+          <GridItem area="aside" paddingX={5}>
+            <GenresList />
+          </GridItem>
+        </Show>
+        <GridItem area="main">
+          <Box paddingLeft={2}>
+            <GameHeading />
+            <Flex marginBottom={5}>
+              <Box marginRight={5}>
+                <PlatformSelector />
+              </Box>
+              <SortSelector />
+            </Flex>
+          </Box>
+          <GameGrid />
+        </GridItem>
+      </Grid>
+      );
+    </div>
+  );
+};
+
+export default HomePage;
