a017ffce2b5e677ff719e1ea6c3d99a1c87adba6
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-03-26 00:52:24 +0100

    Group the Game, Genre and Platform interfaces into separate folder.

--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import genres from "../assets/data/genres";
 import APIClient from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 
 // Staticaly returning the Genres.
 // const useGenres = () => ({ data: genres, isLoading: false, error: null });
