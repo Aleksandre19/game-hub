4834702cada1eba54181ab53c75e1cc156f4679c
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-03-26 03:46:33 +0100

    Refactor entity interfaces.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import getCroppedImage from "../services/imag-url";
 import CriticScore from "./CriticScore";
 import Emoji from "./Emoji";
